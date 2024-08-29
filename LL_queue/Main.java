d71fa2bc27f1f357c727038e50e5d064ad0597f9
RookieProdigy <121886937+AnShihab@users.noreply.github.com>
2024-08-29 22:17:25 +0600

    Main.java

---
+++ LL_queue/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main 
+{
+    public static void main(String[] args) 
+  {
+        Scanner input=new Scanner(System.in);
+        
+        Queue q1=new Queue();
+        q1.enqueue();
+        q1.enqueue();
+        q1.enqueue();
+        q1.enqueue();
+        q1.enqueue();
+        q1.printQueue();
+        q1.dequeue();
+        q1.printQueue();
+        q1.dequeue();
+        q1.printQueue();
+
+    }
+
+}
