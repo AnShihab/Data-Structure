40018f17c86d68fbb2b544c12903a420b5ba2244
RookieProdigy <121886937+AnShihab@users.noreply.github.com>
2024-08-29 22:07:30 +0600

    Main.java

---
+++ LL_Stack/Main.java
@@ -0,0 +1,23 @@
+package Main;
+
+public class Main 
+{
+
+    public static void main(String[] args) 
+    {
+        
+        
+        Stack s=new Stack();
+        s.push();
+        s.push();
+        s.push();
+        s.push();
+        s.push();
+        s.print();
+        s.pop();
+        s.pop();
+        s.print();
+        
+    }
+
+}
