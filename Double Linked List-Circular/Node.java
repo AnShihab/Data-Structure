e2bdb3396086e004aca783971224c4b2feb22291
RookieProdigy <121886937+AnShihab@users.noreply.github.com>
2024-09-10 13:02:52 +0600

    Node.java

--- Double Linked List-Circular/Node.java
+++ Double Linked List-Circular/Node.java
@@ -2,10 +2,4 @@ public class Node {
     int data; 
     Node prev; 
     Node next; 
-
-    public Node(int data) { 
-        this.data = data; 
-        this.prev = null; 
-        this.next = null; 
-    } 
 }
